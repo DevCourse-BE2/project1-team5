63f0be7af14890c53062027d80ff085615f0052d
강바다 <kbd1120@naver.com>
2024-12-01 03:17:05 +0900

    swagger 수정

--- src/main/java/org/example/coffee/global/config/SwaggerConfig.java
+++ src/main/java/org/example/coffee/global/config/SwaggerConfig.java
@@ -3,48 +3,25 @@ package org.example.coffee.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
     // url : http://localhost:8080/swagger-ui/index.html#/
-    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
 
-    @Bean
-    public OpenAPI api() {
-        Server server = new Server().url("/");
-
-        return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
-                .components(authSetting())
-                .info(getSwaggerInfo())
-                .addServersItem(server);
-    }
-
-    private Info getSwaggerInfo() {
-        License license = new License();
-        license.setName("{Application}");
-
+    private Info info() {
         return new Info()
                 .title("Programmers DevCourse Team5 API Document")
                 .description("데브코스 백엔드 Team5 1차 프로젝트 API 문서입니다.")
-                .version("v0.0.1")
-                .license(license);
+                .version("v0.0.1");
     }
 
-    private Components authSetting() {
 
-        return new Components()
-                .addSecuritySchemes(SECURITY_SCHEME_NAME,
-                        new SecurityScheme()
-                                .name(SECURITY_SCHEME_NAME)
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT"));
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(info());
     }
 }
